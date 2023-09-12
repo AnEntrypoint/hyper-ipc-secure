a1d7abbb2d819d65310f9d109554e6b3ca17ea4c
lanmower <almagestfraternite@gmail.com>
2023-09-12 11:26:11 +0000

    dockerserve

---
+++ dockerServe.js
@@ -0,0 +1,32 @@
+const { serve } = require('hyper-ipc');
+module.exports = {
+  init: (kp, Cmd, dockerImage) => {
+    serve(kp, command, async (postData) => {
+      try {
+        const container = await docker.createContainer({
+          Image: dockerImage,
+          Cmd: postData.startCmd,
+          Tty: true,
+          AttachStdin: true,
+          AttachStdout: true,
+          AttachStderr: true,
+        });
+
+        await container.start();
+        const exec = await container.exec({
+          Cmd: postData.Cmd
+        });
+        const stream = await exec.start();
+        const output = await new Promise((resolve, reject) => {
+          container.modem.demuxStream(stream, process.stdout, process.stderr);
+          stream.on('end', () => resolve('Container execution finished.'));
+          stream.on('error', reject);
+        });
+        return output;
+      } catch (error) {
+        console.error('Error:', error);
+        throw error;
+      }
+    });
+  }
+}
